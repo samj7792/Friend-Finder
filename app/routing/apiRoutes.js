fd4034c65fa3bc5f71d9243c3c7a322346b703b5
Sam Jackson <samuelsjackson7@gmail.com>
2019-07-22 18:47:33 -0400

    rough compatibility scores

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,9 +11,40 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function(req,res) {
-        friendsData.push(req.body);
+
+        var newFriend = req.body;
+
+        friendsData.push(newFriend);
         res.json(true);
 
+        var newScores = [];
         
+        for (var i = 0 ; i < newFriend.scores.length ; i++) {
+            newScores.push(parseInt(newFriend.scores[i]));
+        }
+
+        console.log(newScores);
+
+        var bestFriend = {name: "", scores: []};
+
+        for (var i = 0 ; i < friendsData.length - 1 ; i++) {
+            var compat = 0;
+            var compScore = [];
+
+            bestFriend = friendsData[i];
+            console.log(bestFriend);
+            
+            for (var j = 0; j < friendsData[i].scores.length ; j++) {
+                compScore.push(parseInt(friendsData[i].scores[j]));
+            };
+            console.log(compScore);
+
+            for (var j = 0; j < newScores.length; j++) {
+                compat += Math.abs(newScores[j] - compScore[j]);
+            };
+            console.log(compat);
+
+            
+        }
     });
 }
