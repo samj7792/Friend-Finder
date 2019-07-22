7d210806fd5c415a99bbbddc04e13b2b38bb4a0e
Sam Jackson <samuelsjackson7@gmail.com>
2019-07-21 21:37:10 -0400

    added server.js; basic htmlRoutes.js code

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Routes
+
+app.get("*", function(req,res) {
+    res.redirect("/home");
+    res.sendFile(path.join(__dirname,"home.html"));
+});
+
+app.get("/survey", function(req,res) {
+    res.sendFile(path.join(__dirname, "survey.html"));
+});
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
