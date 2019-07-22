2c6e38922d02a626f48fa22b619a5535e82e3a40
Sam Jackson <samuelsjackson7@gmail.com>
2019-07-21 22:22:07 -0400

    fixed apiRoutes logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,28 +1,17 @@
-var express = require("express");
-var path = require("path");
+var friendsData = require ("..data/friends");
 
-var app = express();
-var PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 //Routes
 
-app.get("/api/friends", function(req,res) {
+module.exports = function(app) {
+    
+    app.get("/api/friends", function(req,res) {
+        res.json(friendsData);
+    });
 
-});
-
-app.post("/api/friends", function(req,res) {
-    var newFriend = req.body;
-
-    console.log(newFriend);
-
-    friends.push(newFriend);
-
-    res.json(newFriend);
-});
-
-app.listen(PORT, function(){
-    console.log("App listening on PORT " + PORT);
-});
+    app.post("/api/friends", function(req,res) {
+        friendsData.push(req.body);
+        res.json(true);
+    });
+}
