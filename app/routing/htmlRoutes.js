a02bbc5a3dcd617d2c8a0aba45bdcde3160163f3
Sam Jackson <samuelsjackson7@gmail.com>
2019-07-22 09:28:31 -0400

    added package.json, fixed PORT on server.js, fixed file paths for home.html and survey.html in htmlRoutes.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,10 +5,10 @@ module.exports = function(app) {
 
     // default to home if no matching route
     app.get("*", function(req,res) {
-        res.sendFile(path.join(__dirname,"home.html"));
+        res.sendFile(path.join(__dirname,"../public/home.html"));
     });
 
     app.get("/survey", function(req,res) {
-        res.sendFile(path.join(__dirname, "survey.html"));
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 }
