a0f604ba44ced8f570dc243c0e3ab6723ddab7d6
Sam Jackson <samuelsjackson7@gmail.com>
2019-07-21 22:17:00 -0400

    fixed htmlRoutes logic

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,23 +1,14 @@
-var express = require("express");
 var path = require("path");
 
-var app = express();
-var PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+module.exports = function(app) {
 
-//Routes
+    // default to home if no matching route
+    app.get("*", function(req,res) {
+        res.sendFile(path.join(__dirname,"home.html"));
+    });
 
-app.get("*", function(req,res) {
-    res.redirect("/home");
-    res.sendFile(path.join(__dirname,"home.html"));
-});
-
-app.get("/survey", function(req,res) {
-    res.sendFile(path.join(__dirname, "survey.html"));
-});
-
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-});
+    app.get("/survey", function(req,res) {
+        res.sendFile(path.join(__dirname, "survey.html"));
+    });
+}
