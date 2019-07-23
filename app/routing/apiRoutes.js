5de503e9624aad616451f129b6e6e711cadbb7bd
Sam Jackson <samuelsjackson7@gmail.com>
2019-07-22 22:04:59 -0400

    best match now shows on modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,57 +1,69 @@
 var friendsData = require ("../data/friends");
 
 
-
-//Routes
-
 module.exports = function(app) {
     
+    //Routes
+
     app.get("/api/friends", function(req,res) {
         res.json(friendsData);
     });
 
     app.post("/api/friends", function(req,res) {
 
+        
         var newFriend = req.body;
-
+        
         friendsData.push(newFriend);
-        res.json(true);
-
+        //res.json(true);
+        
         var newScores = [];
         
+        // turns user scores into integers rather than strings
         for (var i = 0 ; i < newFriend.scores.length ; i++) {
             newScores.push(parseInt(newFriend.scores[i]));
         }
-
-        console.log(newScores);
-
+        
+        console.log("------New Entry------");
+        console.log(newFriend);
+        console.log("------Current Users------");
+        
         var bestFriend = {name: "", scores: []};
         var compat = 99;
 
-        for (var i = 0 ; i < friendsData.length - 1 ; i++) {
-            var compScore = [];
-            var nextCompat = 0;
+        bestMatch();
 
-            console.log(friendsData[i]);
-            
-            for (var j = 0; j < friendsData[i].scores.length ; j++) {
-                compScore.push(parseInt(friendsData[i].scores[j]));
-            };
-            console.log(compScore);
-            
-            for (var k = 0; k < newScores.length; k++) {
-                nextCompat += Math.abs(newScores[k] - compScore[k]);
-            };
-            console.log(nextCompat);
-            
-            if (compat > nextCompat) {
-                compat = nextCompat;
-                bestFriend = friendsData[i];
+        res.json(bestFriend);
+
+        function bestMatch() {// find best friend
+            // loop through each in friendsArray except user's input
+            for (var i = 0 ; i < friendsData.length - 1 ; i++) {
+                var compScore = [];
+                var nextCompat = 0;
+
+                console.log(friendsData[i]);
+                
+                // convert friendsData[i].score into integers
+                for (var j = 0; j < friendsData[i].scores.length ; j++) {
+                    compScore.push(parseInt(friendsData[i].scores[j]));
+                };
+                //console.log(compScore);
+                
+                // calculate compatibility
+                for (var k = 0; k < newScores.length; k++) {
+                    nextCompat += Math.abs(newScores[k] - compScore[k]);
+                };
+                //console.log(nextCompat);
+                
+                // save lowest compatibility score
+                if (compat > nextCompat) {
+                    compat = nextCompat;
+                    bestFriend = friendsData[i];
+                }
+                
+                console.log("Best: " + bestFriend.name);
             }
-            
-            console.log("Best: " + bestFriend.name);
-
-            
+            // console.log(bestFriend);
         }
     });
 }
